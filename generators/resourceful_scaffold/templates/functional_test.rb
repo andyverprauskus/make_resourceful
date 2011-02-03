e816f9c2bef8aee60a6e2ac32d8e99762b52a987
Andy Verprauskus <andy@insidejobs.com>
2011-02-03 14:36:15 -0800

    [change] update make_resourceful to use assert_difference, factory_girl and declarative tests

--- generators/resourceful_scaffold/templates/functional_test.rb
+++ generators/resourceful_scaffold/templates/functional_test.rb
@@ -5,45 +5,48 @@ require '<%= controller_file_path %>_controller'
 class <%= controller_class_name %>Controller; def rescue_action(e) raise e end; end
 
 class <%= controller_class_name %>ControllerTest < ActionController::TestCase
+  setup do
+    @@<%= file_name %> = Factory(:<%= file_name %>)
+  end
 
-  def test_should_get_index
+  test "should get index" do
     get :index
     assert_response :success
     assert assigns(:<%= table_name %>)
   end
 
-  def test_should_get_new
+  test "should get new" do
     get :new
     assert_response :success
   end
 
-  def test_should_create_<%= file_name %>
-    old_count = <%= class_name %>.count
-    post :create, :<%= file_name %> => { }
-    assert_equal old_count + 1, <%= class_name %>.count
+  test "should create <%= file_name %>" do
+    assert_difference '<%= class_name %>.count' do
+      post :create, :<%= file_name %> => { }
+    end
 
     assert_redirected_to <%= file_name %>_path(assigns(:<%= file_name %>))
   end
 
-  def test_should_show_<%= file_name %>
-    get :show, :id => 1
+  test "should show <%= file_name %>" do
+    get :show, :id => @@<%= file_name %>.id
     assert_response :success
   end
 
-  def test_should_get_edit
-    get :edit, :id => 1
+  test "should get edit" do
+    get :edit, :id => @@<%= file_name %>.id
     assert_response :success
   end
 
-  def test_should_update_<%= file_name %>
-    put :update, :id => 1, :<%= file_name %> => { }
+  test "should update <%= file_name %>" do
+    put :update, :id => @@<%= file_name %>.id, :<%= file_name %> => { }
     assert_redirected_to <%= file_name %>_path(assigns(:<%= file_name %>))
   end
 
-  def test_should_destroy_<%= file_name %>
-    old_count = <%= class_name %>.count
-    delete :destroy, :id => 1
-    assert_equal old_count-1, <%= class_name %>.count
+  test "should destroy <%= file_name %>" do
+    assert_difference '<%= class_name %>.count', -1 do
+      delete :destroy, :id => @@<%= file_name %>.id
+    end
 
     assert_redirected_to <%= table_name %>_path
   end
